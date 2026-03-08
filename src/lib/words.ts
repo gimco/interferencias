8fefa750a96eba306b54f9ef9052ac44644e6be1
Bruno <gimcoo@gmail.com>
2026-03-08 22:51:28 +0100

    actualizada lista de palabras

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,102 +1,381 @@
 export const secretWords = `
-Alien cocinando
-Alien turista
-Astronauta perdido
-Barco espacial
-Brócoli fuerte
-Brócoli ninja
-Caballero dormido
-Caballero patinador
-Caballo cantante
-Caballo leyendo
-Casa caminando
+*Cantante de pop*
+*Personaje de cine*
+*Político*
+*Profesión*
+Abdominales
+Abejas asesinas
+Acuario
+Acurrucarse
+Afro
+Agua embotellada
+Agua fuerte
+Aguacate
+Ajo en polvo
+Ala delta
+Albornoz
+Alforja
+Alien
+Aliento de perro
+Alta tensión
+Altamira
+Ambulancia
+Amigo
+Ancas de rana
+Antílope
+Análisis de sangre
+Aparcamiento subterráneo
+Apartamento
+Araña
+Arca de Noé
+Ardilla voladora
+Atraco
+Avalancha
+Avispa
+Bailarina
+Bailarina exótica
+Baloncesto
+Banco Santander
+Banquillo de fútbol
+Baqueta
+Barco de vapor
+Barriga
+Bastón
+Bizcocho
+Blackjack
+Boca de incendios
+Bola de cristal
+Bolitas de queso
+Bollicao
+Boquerones en vinagre
+Borrego
+Botas cowboy
+Bugs Bunny
+Cabina de avión
+Cabra
+Cacerola
+Calamar
+Calefactor
+Calentamiento global
+Camello
+Camino de Santiago
+Camiseta hawaiana
+Camión cisterna
+Campamento de verano
+Canario
+Capullo de rosa
+Caracol
+Carnaval
+Carrera de caballos
+Carrera de galgos
+Carretera de montaña
+Casa encantada
 Casa flotante
-Cerdo rey
-Cerdo volador
-Coche volador
-Conejo boxeador
-Conejo mago
-Dinosaurio cocinero
-Dinosaurio patinando
-Donut gigante
-Donut nadador
-Dragón durmiendo
-Dragón panadero
-Elefante bailarín
-Elefante pequeño coche
-Fantasma bailando
-Fantasma lector
-Gallina rockera
-Gato astronauta
-Gato con corona
-Gato pirata
-Hamburguesa bailando
-Hamburguesa corriendo
-Helado corriendo
-Helado triste
-Huevo asustado
-Huevo ciclista
-Isla flotante
-Isla voladora
-Jirafa dormida
-Jirafa en ascensor
-León pintor
-Luna cantante
-Luna comiendo
-Mago despistado
-Mono detective
-Monstruo debajo cama
-Montaña bailando
-Montaña sonriente
-Ninja comiendo pizza
-Ninja dormido
-Niño robot
-Nube enfadada
-Nube llorando
-Oveja astronauta
-Oveja DJ
-Panda cocinero
-Panda karate
-Patata astronauta
-Patata rockera
-Pato detective
-Pato mecánico
-Perro chef
-Perro con gafas
-Perro pirata
-Pingüino astronauta
-Pingüino cantante
-Pirata dormilón
-Pirata pescando
-Pizza cantante
-Pizza voladora
-Plátano espía
-Plátano superhéroe
-Pulpo batería
-Pulpo pintor
-Rana astronauta
-Rana rey
-Robot jardinero
-Robot llorando
-Sandía boxeadora
-Sandía cantante
-Sol con gafas
-Sol dormido
-Taco bailarín
-Taco volador
-Tiburón bailarín
-Tiburón cantante
-Tortuga ninja
-Tortuga piloto
-Unicornio policía
-Vaca bailando
-Vaca surfista
-Vikingo guitarrista
-Vikingo surfista
-Volcán enfadado
-Volcán feliz
-Zombie ciclista
-Zombie estudiando
-Árbol bailarín
-Árbol con ojos
+Cascada
+Castor
+Catapulta
+Cencerro
+Cerdo enano
+Cerezo
+Cerillas
+Cerumen
+Chatarrería
+Chef
+Chilena
+Chiquito de la Calzada
+Choca esos cinco
+Chocolate caliente
+Chupa de cuero
+Ciempiés
+Ciervo
+Circo de pulgas
+Cisne
+Coche blindado
+Cochinillo de Segovia
+Cola de caballo
+Coliflor
+Collar de perlas
+Comedero de pájaros
+Comida casera
+Como pez fuera del agua
+Compañero de habitación
+Contenedor de vidrio
+Copos de maíz
+Costillas a la barbacoa
+Coyote
+Crema catalana
+Crema corporal
+Cruasán
+Críquet
+Cucharada
+Cuidadora de niños
+Culturista
+Cóctel de gambas
+Darth Vader
+Disco
+Disco duro
+Diseñador de moda
+Drácula
+Dumbo
+Día de la marmota
+Eclipse
+El amor es ciego
+Enamorado
+Erizo de mar
+Escayola
+Espagueti
+Espárragos
+Esquimal
+Estadio de béisbol
+Euros
+Falda escocesa
+Familia real
+Faquir
+Fiesta de la cerveza
+Flor de alhelí
+Fontanero
+Fotógrafo
+Fuegos artificiales
+Funambulista
+Gafas
+Gafas de esquí
+Gafas de protección
+Gaita
+Galleta de la suerte
+Gatito
+Genio de la lámpara
+Glotón
+Golfo de México
+Goma elástica
+Gorila
+Gran Canaria
+Gran Cañón del Colorado
+Grandes almacenes
+Granjero
+Gravedad
+Grease
+Gremlin
+Guantes de goma
+Guerra de bolas de nieve
+Habitación de hospital
+Hada madrina
+Helado de fresa
+Hermanastro
+Hervido
+Hipopótamo
+Hipotermia
+Hombre al agua
+Hombre en la luna
+Hombre rana
+Hora del té
+Hora punta
+Hormiga roja
+Hostal
+Huella dactilar
+Huesos de santo
+Huevo de perdiz
+Huevo podrido
+Huevos revueltos
+Hámster
+Iceberg
+Infantil
+Informático
+Intestinos
+Invernadero
+Isla de Pascua
+Jacuzzi
+Jamón ibérico
+Japón
+Jardín
+José Luis Rodríguez Zapatero
+La Cibeles
+La cima del mundo
+La gallinita ciega
+La guerra de las galaxias
+La jungla de cristal
+Ladrón de guante blanco
+Lanzarote
+Lavavajillas
+Lechuza
+Leotardos
+Licencia de pesca
+Lifting
+Lince
+Llave inglesa
+Llavero
+Lluvia tropical
+Luchador de sumo
+Lápida
+Mago de Oz
+Mal aliento
+Maletín
+Mallorca
+Mamut
+Maniquí
+Manitas
+Mapa del tesoro
+Mareado
+Margarita
+Mariposa monarca
+Marquesina
+Martes y trece
+Matamoscas
+Mazmorra
+Mendigar
+Meter la pata
+Minigolf
+Misión imposible
+Mofeta
+Mona Lisa
+Monja
+Monstruo de las galletas
+Motocicleta
+Motor 6 cilindros
+Muelle portuario
+Mujer barbuda
+Músculo
+Música de ascensor
+Navaja
+Nido vacío
+Orujo
+Oráculo
+Osito de peluche
+Oso hormiguero
+Oso Yogui
+Padre de la novia
+Padrino de boda
+Paletilla de cordero
+Paloma de la paz
+Palomitas de maíz
+Pan integral
+Panda
+Pantalones de boxeador
+Pantalones de pana
+Pantalones pirata
+Papá Noel
+Parada de autobús
+Parar un penalti
+Parque de bolas
+Paseo por el parque
+Pastel de cumpleaños
+Pastel de frutas
+Pastel de manzana
+Pastel de queso
+Pastilla para la tos
+Pato Lucas
+Pavo ahumado
+Payaso
+Pañal
+Pecera
+Pecera de tortugas
+Pechuga de pollo
+Pectorales
+Pegamento de barra
+Pelota de goma
+Pelota de playa
+Pelota vasca
+Peluquero
+Pelícano
+Película de carreras
+Perrito de la pradera
+Perro caniche
+Perro policía
+Persona famosa
+Pez globo
+Pez luna
+Phoskitos
+Pies fríos
+Pirineos
+Pisa
+Pizza congelada
+Piña colada
+Plató de televisión
+Pluto
+Pollo frito
+Polución
+Portero de fútbol
+Primo
+Princesa de Asturias
+Princesa Rapunzel
+Psicólogo
+Puente levadizo
+Puesta de sol
+Pulpo a la gallega
+Pájaro loco
+Póker
+Queso de untar
+Queso manchego
+Raqueta de bádminton
+Rayuela
+Recepción de hotel
+Reciclar
+Refugio subterráneo
+Reloj de pared
+Remolino de agua
+Rendirse
+Retorcer un brazo
+Rinoceronte
+Rodeo
+Roma
+Romperse una pierna
+Rueda
+Rueda de reconocimiento
+Rusia
+Sala de espera
+Salsa
+Salsa de tomate
+San Francisco
+Sapo
+Satélite
+Semáforo
+Sepulturero
+Sillón de dentista
+Sitio famoso
+Snoopy
+Sobaos
+Solomillo a la brasa
+Sombrero
+Soplador de vidrio
+Soplete
+Squash
+Submarino
+Sushi
+Sándwich de huevo
+Tabla de snow
+Tarjeta de crédito
+Tarta de Santiago
+Teletienda
+Tenia solitaria
+Tiburón blanco
+Tienda de alimentos
+Timbre de puerta
+Tintorería
+Tirachinas
+Tirantes
+Toallas de papel
+Top model
+Tortilla de patatas
+Tortilla francesa
+Traje a medida
+Trampa
+Trampolín
+Trasplante
+Tren de la bruja
+Tren de mercancías
+Trenza
+Unicornio
+Vendedor de coches
+Veneno
+Venus de Milo
+Verano azul
+Viento favorable
+Viernes Santo
+Visión de rayos X
+Vitaminas
+Volcán
+Yoga
+Zona horaria
+Zoo
+Zumo de uva
+Árbol de la granja
 `.trim().split('\n');
