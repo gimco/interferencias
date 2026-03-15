e020d5086f6d6c7dad5281322d1d75f91f7d0810
Bruno <gimcoo@gmail.com>
2026-03-15 13:29:28 +0100

    agregados resto de palabras

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,59 +1,123 @@
 export const secretWords = `
 *Cantante de pop*
+*Comida favorita*
+*Concurso de televisión*
+*Deporte de equipo*
 *Personaje de cine*
 *Político*
 *Profesión*
+*Programa infantil*
+*Superhéroe*
+*Videojuego*
+12 de octubre
 Abdominales
+Abeja
 Abejas asesinas
+Abrazo
+Abrebotellas
 Acuario
 Acurrucarse
+Admirador secreto
+Adolescente
 Afro
 Agua embotellada
 Agua fuerte
+Agua salada
 Aguacate
+Ajedrez
 Ajo en polvo
 Ala delta
 Albornoz
+Alfombra voladora
 Alforja
+Algodón de azúcar
 Alien
 Aliento de perro
 Alta tensión
 Altamira
+Ama de casa
 Ambulancia
 Amigo
+Amigo imaginario
 Ancas de rana
+Andar de puntillas
 Antílope
 Análisis de sangre
+Aparcamiento de emergencia
 Aparcamiento subterráneo
 Apartamento
+Apio
+Apresa-perros
+Apéndice
 Araña
 Arca de Noé
 Ardilla voladora
+Armadillo
+Aros de cebolla
+Arrullar
+Artista callejero
+Astronauta
 Atraco
+Autobús de dos pisos
+Autopsia
 Avalancha
+Avestruz
 Avispa
+Año nuevo
+Bache
 Bailarina
 Bailarina exótica
 Baloncesto
+Bambú
+Banana split
+Banco
+Banco del parque
 Banco Santander
+Banda de rock
 Banquillo de fútbol
 Baqueta
+Barca de remos
 Barco de vapor
+Barco encallado
 Barriga
+Barriga cervecera
+Barril de cerveza
+Barrita energética
 Bastón
+Batman
+Baño caliente
+Beicon con huevos
+Bella durmiente
+Beso francés
 Bizcocho
 Blackjack
+Blancanieves
+Bob Esponja
 Boca de incendios
+Boda
 Bola de cristal
+Bola de pelo
 Bolitas de queso
 Bollicao
 Boquerones en vinagre
 Borrego
 Botas cowboy
+Botones de hotel
+Brazo de gitano
+Brillo de labios
+Brócoli
+Bróker financiero
+Buceo a pulmón
+Buffet libre
 Bugs Bunny
+Burro
 Cabina de avión
 Cabra
+Cacatúa
 Cacerola
+Cadena de perro
+Caja de herramientas
+Calabaza de Halloween
 Calamar
 Calefactor
 Calentamiento global
@@ -61,77 +125,166 @@ Camello
 Camino de Santiago
 Camiseta hawaiana
 Camión cisterna
+Camión de bomberos
+Camión de helados
 Campamento de verano
+Campanilla
+Campo de fútbol
+Campo de golf
 Canario
+Canasta de 3 puntos
+Canoa
 Capullo de rosa
 Caracol
 Carnaval
 Carrera de caballos
 Carrera de galgos
 Carretera de montaña
+Carrito de golf
+Cartas de los fans
+Casa de la abuela
+Casa de muñecas
+Casa de verano
+Casa del árbol
 Casa encantada
 Casa flotante
 Cascada
+Caseta de peaje
+Castillo de arena
 Castor
 Catapulta
+Caviar
 Cencerro
+Cenicienta
 Cerdo enano
+Cerebro de mosquito
 Cerezo
 Cerillas
 Cerumen
+Chaflán
+Chaleco antibalas
+Champiñón
+Charco
 Chatarrería
 Chef
+Chicle
+Chico de calendario
+Chihuahua
 Chilena
+Chimpancé
 Chiquito de la Calzada
 Choca esos cinco
 Chocolate caliente
+Chucherías
 Chupa de cuero
+Chuparse los dedos
+Chuparse los dedos
 Ciempiés
 Ciervo
+Cinturón de asteroides
 Circo de pulgas
 Cisne
+Cita a ciegas
+Cobra
 Coche blindado
+Coche de carreras
+Coche de choque
+Coche fúnebre
 Cochinillo de Segovia
 Cola de caballo
 Coliflor
 Collar de perlas
+Columpio
 Comedero de pájaros
+Comerse una vaca
 Comida casera
+Comida de perros
+Comida rápida
+Como el gato y el ratón
 Como pez fuera del agua
 Compañero de habitación
+Concha marina
+Concha marina
+Concierto de rock
+Conductor de autobús
 Contenedor de vidrio
+Control remoto
 Copos de maíz
+Cortada de queso
 Costillas a la barbacoa
 Coyote
 Crema catalana
 Crema corporal
+Cristóbal Colón
 Cruasán
 Críquet
 Cucharada
+Cuervo
 Cuidadora de niños
+Culebrón
 Culturista
+Cuna
+Cura
+Cárcel
+Cíclope
 Cóctel de gambas
+Dama de honor
 Darth Vader
+Dedo anular
+Dedo gordo del pie
+Delfín
+Descapotable
+Desfile
+Despacho
+Detector de metales
+Dientes postizos
+Dinamita
 Disco
 Disco duro
 Diseñador de moda
+Doblar una esquina
+Doctor
+Donut
 Drácula
 Dumbo
 Día de la marmota
 Eclipse
+Edredón
 El amor es ciego
+El Palacio Real
+El patito feo
+Electricista
 Enamorado
+Encima de la valla
+Enjuague bucal
+Ensalada de arroz
 Erizo de mar
+Escaparse
 Escayola
+Escorpión
+Esgrima
+Espacio exterior
 Espagueti
+Espaguetis boloñesa
+Esparadrapo
+Espejismo
+Esposas
 Espárragos
+Espía doble
 Esquimal
+Esquina de calle
 Estadio de béisbol
+Esteticista
+Estornudar
 Euros
+Fajita
 Falda escocesa
 Familia real
 Faquir
+Fiebre amarilla
 Fiesta de la cerveza
+Fiesta sorpresa
+Flauta
 Flor de alhelí
 Fontanero
 Fotógrafo
@@ -142,12 +295,23 @@ Gafas de esquí
 Gafas de protección
 Gaita
 Galleta de la suerte
+Gallo
+Gamba
+Ganadero
 Gatito
+Gato con botas
+Gato gordo
+Gemelos
 Genio de la lámpara
+Gimnasta
+Giro 180°
 Glotón
+Gofre de chocolate
 Golfo de México
 Goma elástica
+Gong
 Gorila
+Gorra de pensar
 Gran Canaria
 Gran Cañón del Colorado
 Grandes almacenes
@@ -155,227 +319,454 @@ Granjero
 Gravedad
 Grease
 Gremlin
+Guacamole
 Guantes de goma
+Guarda del zoo
+Guardabosques
+Guardaespaldas
+Guardería
 Guerra de bolas de nieve
+Guisante
+Gánster
 Habitación de hospital
+Hablar por los codos
+Hacer autostop
 Hada madrina
+Hawái
 Helado de fresa
+Herederos
 Hermanastro
+Hermano mediano
 Hervido
+Higadillos
+Hija
 Hipopótamo
 Hipotermia
+Hockey sobre ruedas
+Hollywood
 Hombre al agua
+Hombre del tiempo
 Hombre en la luna
 Hombre rana
+Hombreras
+Hommer Simpson
 Hora del té
 Hora punta
 Hormiga roja
+Hormiguero
+Hospital veterinario
 Hostal
+Hucha
 Huella dactilar
 Huesos de santo
 Huevo de perdiz
+Huevo duro
+Huevo frito
 Huevo podrido
 Huevos revueltos
 Hámster
 Iceberg
+Indio americano
 Infantil
 Informático
+Instituto
+Intermedio
 Intestinos
 Invernadero
 Isla de Pascua
+Isla desierta
 Jacuzzi
+Jamón en lata
 Jamón ibérico
+Jamón serrano
 Japón
 Jardín
+Jaula del zoo
+Jeroglífico
 José Luis Rodríguez Zapatero
+Jungla
+Júpiter
 La Cibeles
 La cima del mundo
 La gallinita ciega
 La guerra de las galaxias
 La jungla de cristal
+La pajita más larga
+Laberinto
 Ladrón de guante blanco
+Lagaña
 Lanzarote
+Lata de sardinas
 Lavavajillas
+Lechero
+Lecho del río
+Lechuga iceberg
 Lechuza
+Lengua trabada
+Leopardo
 Leotardos
+León marino
+Libélula
 Licencia de pesca
 Lifting
+Limpia alfombras
 Lince
+Linterna
+Liquidez
 Llave inglesa
 Llavero
+Llueven rayos y truenos
 Lluvia tropical
+Lobo
+Lombriz de tierra
+Los tres cerditos
+Lotería
+Lucha de pulgares
 Luchador de sumo
+Luciérnaga
 Lápida
+Madrid
 Mago de Oz
 Mal aliento
+Mala hierba
 Maletín
 Mallorca
 Mamut
+Mamá gallina
 Maniquí
 Manitas
+Manzana podrida
 Mapa del tesoro
 Mareado
 Margarita
+Marihuana
+Marinero
+Mariposa
 Mariposa monarca
+Mariquita
 Marquesina
 Martes y trece
 Matamoscas
+Mayonesa
 Mazmorra
+Medusa
+Melón con jamón
 Mendigar
+Mercadillo
+Meteoro
 Meter la pata
 Minigolf
 Misión imposible
+Mochila
 Mofeta
+Momia
 Mona Lisa
+Monasterio
 Monja
+Mono
+Monstruo
 Monstruo de las galletas
+Monte Everest
+Morderse las uñas
+Mordisco
+Mosca común
+Mosca de la fruta
+Moto de nieve
 Motocicleta
 Motor 6 cilindros
 Muelle portuario
 Mujer barbuda
+Multa por velocidad
+Murciélago vampiro
+Murciélago vampiro
+Máquina de pinball
+Máscara
 Músculo
 Música de ascensor
+Nachos
+Nata montada
 Navaja
+Nave espacial
 Nido vacío
+Ninja
+Niños
+Nueva Jersey
+Nueva York
+Nuevo Méjico
+Oasis
+Océano Atlántico
+Océano Pacífico
+Ojo de cristal
+Ojos abiertos
 Orujo
 Oráculo
 Osito de peluche
 Oso hormiguero
+Oso pardo
 Oso Yogui
 Padre de la novia
+Padrino
 Padrino de boda
+Paella
+Pajarera
 Paletilla de cordero
 Paloma de la paz
 Palomitas de maíz
 Pan integral
 Panda
+Pandereta
 Pantalones de boxeador
+Pantalones de cuero
 Pantalones de pana
 Pantalones pirata
+Papel de aluminio
+Papel de váter
+Papel pintado
 Papá Noel
 Parada de autobús
 Parar un penalti
+Pararrayos
+Parlamento
 Parque de bolas
+Paseo marítimo
 Paseo por el parque
 Pastel de cumpleaños
 Pastel de frutas
 Pastel de manzana
 Pastel de queso
 Pastilla para la tos
+Patata asada
+Patito de goma
 Pato Lucas
+Pato pekín
 Pavo ahumado
 Payaso
+Payaso de la clase
 Pañal
+Pecas
 Pecera
 Pecera de tortugas
 Pechuga de pollo
 Pectorales
+Pedir una cita
 Pegamento de barra
+Peli en casa
 Pelota de goma
 Pelota de playa
+Pelota del profesor
 Pelota vasca
 Peluquero
+Peluquín
 Pelícano
 Película de carreras
+Penalti
+Percebe
+Perilla
+Perrera
 Perrito de la pradera
+Perro
 Perro caniche
+Perro pastor
 Perro policía
 Persona famosa
+Pesadilla
+Pez espada
 Pez globo
 Pez luna
+Pez payaso
 Phoskitos
+Piel de gallina
+Piel de la naranja
 Pies fríos
+Pinzas de depilar
+Piraña
 Pirineos
 Pisa
 Pizza congelada
 Piña colada
+Planta nuclear
 Plató de televisión
 Pluto
+Polilla
 Pollo frito
 Polución
 Portero de fútbol
+Posavasos
+Poste de teléfono
+Presidente
 Primo
 Princesa de Asturias
 Princesa Rapunzel
+Protector solar
 Psicólogo
 Puente levadizo
 Puesta de sol
+Pulga
+Pulgares arriba
+Pulmones
+Pulmones
 Pulpo a la gallega
+Pulso gitano
+Punto de encuentro
 Pájaro loco
 Póker
+Púa de guitarra
+Quemadura del sol
+Queso de cabra
 Queso de untar
+Queso fundido
 Queso manchego
 Raqueta de bádminton
+Raya marina
+Rayos X
 Rayuela
 Recepción de hotel
 Reciclar
+Reclinatorio
+Refugio de montaña
 Refugio subterráneo
+Reina de la belleza
+Reloj de cuco
 Reloj de pared
 Remolino de agua
 Rendirse
+Repartidor de periódicos
+Residuo tóxico
 Retorcer un brazo
+Rey de la jungla
 Rinoceronte
+Rizador de pestañas
+Robot
+Rock 'n' roll
 Rodeo
 Roma
 Romperse una pierna
 Rueda
 Rueda de reconocimiento
+Rueda pinchada
 Rusia
+Río Amazonas
+Saco de boxeo
+Safari
+Sal
+Sala de cine
+Sala de emergencias
 Sala de espera
+Salida de emergencia
+Salmón
 Salsa
 Salsa de tomate
+Saltamontes
+Salto al vacío
+Salto de altura
+San Bernardo
 San Francisco
+Sandía
+Sanguijuela
 Sapo
 Satélite
+Semana Santa
 Semáforo
 Sepulturero
+Sheriff
+Siesta
+Silla de ruedas
 Sillón de dentista
+Sirvienta
 Sitio famoso
 Snoopy
 Sobaos
+Soldado
 Solomillo a la brasa
+Sombra de ojos
 Sombrero
+Sombrero viejo
 Soplador de vidrio
 Soplete
+Soñar despierto
+Spiderman
 Squash
 Submarino
+Suegra
+Suegro
 Sushi
+Sándwich de atún
 Sándwich de huevo
 Tabla de snow
+Tamaño XXXL
+Tanque de tiburones
 Tarjeta de crédito
+Tarta de manzana
 Tarta de Santiago
+Tatuaje
+Taza de café
+Telaraña
+Telescopio
 Teletienda
+Teléfono de pago
 Tenia solitaria
+Tentáculo
+Terremoto
 Tiburón blanco
 Tienda de alimentos
+Tienda de empeños
+Tienda de mascotas
+Tigre
 Timbre de puerta
 Tintorería
+Tira de cómic
 Tirachinas
 Tirantes
 Toallas de papel
 Top model
 Tortilla de patatas
 Tortilla francesa
+Tostadora
+Trabalenguas
+Tragaperras
+Tragaperras
 Traje a medida
+Traje de boda
 Trampa
 Trampolín
 Trasplante
 Tren de la bruja
 Tren de mercancías
 Trenza
+Trineo de nieve
+Trombón
+Trompeta
+Trompeta
+Trono
+Trébol de 4 hojas
+Ubre
+Una aguja en un pajar
+Una mosca en la sopa
 Unicornio
+Vaca loca
+Vacaciones de Pascua
+Vaquero
+Vecino
 Vendedor de coches
 Veneno
+Ventilador de techo
 Venus de Milo
 Verano azul
+Vestuario
+Vicepresidente
 Viento favorable
 Viernes Santo
 Visión de rayos X
 Vitaminas
 Volcán
+Voleibol
+Volquete
+Vudú
 Yoga
 Zona horaria
 Zoo
+Zumo de manzana
 Zumo de uva
+Águila real
 Árbol de la granja
 `.trim().split('\n');
